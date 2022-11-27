0d2dbae1336895468aed968591cb687bdd8669c7
ozzy <ozzy@cydeo.com>
2022-11-27 13:09:30 -0500

    Spring REST

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -27,5 +27,30 @@ public class CourseController {
         return courseService.getCourseById(courseId);
     }
 
+    @GetMapping("category/{name}")
+    public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category){
+        return courseService.getCoursesByCategory(category);
+
+    }
+
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO course){
+        return courseService.createCourse(course);
+    }
+
+    @PutMapping("{id}")
+    public void updateCourse(@PathVariable("id") long courseId,@RequestBody CourseDTO course){
+        courseService.updateCourse(courseId,course);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCourseById(@PathVariable("id") long courseId){
+        courseService.deleteCourseById(courseId);
+
+    }
+
+
+
+
 
 }
