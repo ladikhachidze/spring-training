5bc219d2ae09254fd5fee7a90ebfd7a20035f162
CundullahT-CT <cterzioglu@cybertekschool.com>
2022-11-28 21:21:41 +0100

    adds database layer to spring-17-REST-ConsumingApis

--- spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -19,4 +20,10 @@ public class Spring17RestConsumingApisApplication {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+
 }
