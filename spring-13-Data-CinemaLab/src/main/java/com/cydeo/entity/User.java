dc70b76dac313ae2a2914fb0156f8d89b5ea3266
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-04-10 13:24:25 -0400

    cinema lab

---
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,16 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User extends BaseEntity{
+
+
+
+
+}
