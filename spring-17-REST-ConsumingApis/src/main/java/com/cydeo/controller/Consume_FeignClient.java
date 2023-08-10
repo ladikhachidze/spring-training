973ad11720e1e7ffb59df87d08c1d9ab877302d9
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-08-10 16:16:56 -0400

    review

--- spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_FeignClient.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_FeignClient.java
@@ -1,35 +1,27 @@
 package com.cydeo.controller;
 
-import com.cydeo.client.EmployeeClient;
 import com.cydeo.client.UserClient;
 import com.cydeo.dto.ResponseWrapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/users")
 public class Consume_FeignClient {
 
     private final UserClient userClient;
-    private final EmployeeClient employeeClient;
 
-    public Consume_FeignClient(UserClient userClient, EmployeeClient employeeClient) {
+    public Consume_FeignClient(UserClient userClient) {
         this.userClient = userClient;
-        this.employeeClient = employeeClient;
     }
 
-    @GetMapping("/api/v1/users")
+
+    @GetMapping()
     public ResponseEntity<ResponseWrapper> getUsers(){
 
         return ResponseEntity.ok(new ResponseWrapper("UserList Retrieved",userClient.getUsers()));
     }
-
-    @GetMapping("/api/v1/employee")
-    public ResponseEntity<ResponseWrapper> getEmployee(){
-
-        return ResponseEntity.ok(new ResponseWrapper("Employee retrieved",employeeClient.getEmployee("6298ebfecd0551211fce37a6")));
-
-    }
-
 
 }
